4cbbfb0ec9b1bf10d0c6de2cb2a556aa761d2dab
Matheus <matheusf06@gmail.com>
2021-08-18 18:32:02 -0300

    fix port

--- index.js
+++ index.js
@@ -46,7 +46,9 @@ const init = async() => {
 };
 init();
 
-app.listen(3000, err => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, err => {
     if(err){
         console.log("Deu erro")
     }else{
